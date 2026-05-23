7afe95d5da6cfd12297230756f2681d1bcfca082
tanmaybiswas-creator <tanmaybiswas@in.ibm.com>
2026-05-23 13:13:01 +0530

    updated file

--- aichamp_demo_3rd.py
+++ aichamp_demo_3rd.py
@@ -3,4 +3,16 @@ number = int(input("Enter a number: "))
 if number % 2 == 0:
     print(f"{number} is even.")
 else:
-    print(f"{number} is odd.")
+    is_prime = True
+    if number < 2:
+        is_prime = False
+    else:
+        for i in range(2, int(number**0.5) + 1):
+            if number % i == 0:
+                is_prime = False
+                break
+
+    if is_prime:
+        print(f"{number} is odd and prime.")
+    else:
+        print(f"{number} is odd but not prime.")
